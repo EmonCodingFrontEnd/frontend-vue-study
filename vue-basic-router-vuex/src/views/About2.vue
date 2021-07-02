a068e512f4d09a5f83628d1b1b6bdb1067a89b2c
emon <liming20110711@163.com>
2021-07-02 23:57:12 +0800

    update:20210702-03

---
+++ vue-basic-router-vuex/src/views/About2.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="about">
+    <h1 @click="handleClickSync">This is an about page</h1>
+    <h1 @click="handleClickASync">{{ name }}</h1>
+
+  </div>
+</template>
+
+<script>
+import { useStore } from 'vuex'
+import { toRefs } from 'vue'
+
+export default {
+  name: 'About2',
+  setup () {
+    const store = useStore()
+    const { name } = toRefs(store.state)
+
+    const handleClickSync = () => {
+      store.commit('change')
+    }
+
+    const handleClickASync = () => {
+      store.dispatch('change', 'hello world!')
+    }
+
+    return {
+      name,
+      handleClickSync,
+      handleClickASync
+    }
+  }
+}
+</script>
---
+++ vue-basic-router-vuex/src/views/Home2.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <h1>{{ name }}</h1>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+
+import { useStore } from 'vuex'
+import { toRefs } from 'vue'
+
+export default {
+  name: 'Home2',
+  setup () {
+    const store = useStore()
+    const { name } = toRefs(store.state)
+
+    return {
+      name
+    }
+  }
+}
+</script>
