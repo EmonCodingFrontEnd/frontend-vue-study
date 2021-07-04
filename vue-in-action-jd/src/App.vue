e735b7d80967296a8979af69db0a2ec06f3882ed
emon <liming20110711@163.com>
2021-07-04 11:18:53 +0800

    update:20210704-01:首页底部完成，CSS已经sass化

--- vue-in-action-jd/src/App.vue
+++ vue-in-action-jd/src/App.vue
@@ -1,20 +1,24 @@
 <template>
   <div class="docker">
-    <span class="docker__item">
+    <!--
+    BEM CSS
+    block__element--Modifier
+    -->
+    <span class="docker__item docker__item--active">
       <div class="iconfont">&#xe60b;</div>
-      首页
+      <div class="docker_title">首页</div>
     </span>
     <span class="docker__item">
       <div class="iconfont">&#xec90;</div>
-      购物车
+      <div class="docker_title">购物车</div>
     </span>
     <span class="docker__item">
       <div class="iconfont">&#xe61e;</div>
-      订单
+      <div class="docker_title">订单</div>
     </span>
     <span class="docker__item">
       <div class="iconfont">&#xe61f;</div>
-      我的
+      <div class="docker_title">我的</div>
     </span>
   </div>
 </template>
@@ -30,10 +34,46 @@
   width: 100%;
   height: 0.49rem;
   border-top: 1px solid #F1F1F1;
+  //background: green;
+
+  &__item {
+    flex: 1;
+    text-align: center;
+
+    .iconfont {
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
+    }
+
+    &--active {
+      color: #1FA4FC;
+    }
+  }
+
+  &__title {
+    font-size: 20px;
+    transform: scale(.5, .5);
+    transform-origin: center top;
+  }
 }
 
-.docker__item {
+/*.docker__item {
   flex: 1;
   text-align: center;
-}
+
+  .iconfont {
+    margin: .07rem 0 .02rem 0;
+    font-size: .18rem;
+  }
+}*/
+
+/*.docker__item--active {
+  color: #1FA4FC;
+}*/
+
+/*.docker_title {
+  font-size: 20px;
+  transform: scale(.5, .5);
+  transform-origin: center top;
+}*/
 </style>
