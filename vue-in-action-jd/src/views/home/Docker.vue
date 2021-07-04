38cd1b717e3c045eb36557f5b229e7d8218319a5
emon <liming20110711@163.com>
2021-07-04 19:13:43 +0800

    update:20210704-07:首页布局拆分组件完成

---
+++ vue-in-action-jd/src/views/home/Docker.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="docker">
+    <!--
+    BEM CSS
+    block__element--Modifier
+    -->
+    <div class="docker__item docker__item--active">
+      <div class="iconfont">&#xe60b;</div>
+      <div class="docker_title">首页</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xec90;</div>
+      <div class="docker_title">购物车</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe61e;</div>
+      <div class="docker_title">订单</div>
+    </div>
+    <div class="docker__item">
+      <div class="iconfont">&#xe61f;</div>
+      <div class="docker_title">我的</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Docker'
+}
+</script>
+
+<style lang="scss">
+@import '../../style/viriables.scss';
+@import '../../style/mixins.scss';
+
+.docker {
+  display: flex;
+  box-sizing: border-box;
+  position: absolute;
+  padding: 0 .18rem;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 0.49rem;
+  border-top: 1px solid $content-bgColor;
+  color: $content-fontcolor;
+  //background: green;
+
+  &__item {
+    flex: 1;
+    text-align: center;
+
+    .iconfont {
+      margin: .07rem 0 .02rem 0;
+      font-size: .18rem;
+    }
+
+    &--active {
+      color: #1FA4FC;
+    }
+  }
+
+  &__title {
+    font-size: .2rem;
+    transform: scale(.5, .5);
+    transform-origin: center top;
+  }
+}
+
+/*.docker__item {
+  flex: 1;
+  text-align: center;
+
+  .iconfont {
+    margin: .07rem 0 .02rem 0;
+    font-size: .18rem;
+  }
+}*/
+
+/*.docker__item--active {
+  color: #1FA4FC;
+}*/
+
+/*.docker_title {
+  font-size: 20px;
+  transform: scale(.5, .5);
+  transform-origin: center top;
+}*/
+</style>
