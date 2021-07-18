1628bf004b5462b547384de450e8896ca826dfa8
emon <liming20110711@163.com>
2021-07-18 23:02:05 +0800

    update:20210718-08:商家页面核心样式开发，样式的优化与代码复用

---
+++ vue-in-action-jd/src/views/shop/Content.vue
@@ -0,0 +1,153 @@
+<template>
+  <div class="content">
+    <div class="category">
+      <div class="category__item category__item--active">全部商品</div>
+      <div class="category__item">秒杀</div>
+      <div class="category__item">新鲜水果</div>
+      <div class="category__item">休闲食品</div>
+      <div class="category__item">时令蔬菜</div>
+      <div class="category__item">肉蛋家禽</div>
+    </div>
+    <div class="product">
+      <div class="product__item">
+        <img class="product__item__img" src="http://www.dell-lee.com/imgs/vue3/near.png" alt="1">
+        <div class="product__item__detail">
+          <h4 class="product__item__title">番茄250g/份</h4>
+          <p class="product__item__sales">月售10件</p>
+          <p class="product__item__price">
+            <span class="product__item__yen">&yen;</span>33.6
+            <span class="product__item__origin">&yen;66.6</span>
+          </p>
+        </div>
+        <div class="product__number">
+          <div class="product__number__minus">-</div>
+          0
+          <div class="product__number__plus">+</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Content'
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../style/viriables";
+@import "../../style/mixins";
+
+.content {
+  display: flex;
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 1.5rem;
+  bottom: .5rem;
+}
+
+.category {
+  overflow-y: scroll;
+  height: 100%;
+  width: .76rem;
+  background: #F5F5F5;
+
+  &__item {
+    line-height: .4rem;
+    text-align: center;
+    font-size: .14rem;
+    color: $content-fontcolor;
+
+    &--active {
+      background: $bgColor;
+    }
+  }
+}
+
+.product {
+  overflow-y: scroll;
+  flex: 1;
+
+  &__item {
+    position: relative;
+    display: flex;
+    padding: .12rem 0;
+    margin: 0 .16rem;
+    border-bottom: .01rem solid $content-bgColor;
+
+    &__img {
+      width: .68rem;
+      height: .68rem;
+      margin-right: .16rem;
+    }
+
+    &__detail {
+      overflow: hidden;
+    }
+
+    &__title {
+      margin: 0;
+      line-height: .2rem;
+      font-size: .14rem;
+      color: $content-fontcolor;
+      @include ellipsis
+    }
+
+    &__sales {
+      margin: .06rem 0;
+      line-height: .16rem;
+      font-size: .12rem;
+      color: $content-fontcolor;
+    }
+
+    &__price {
+      margin: 0;
+      line-height: .2rem;
+      font-size: .14rem;
+      color: $hightlight-fontColor;
+    }
+
+    &__yen {
+      font-size: .12rem;
+    }
+
+    &__origin {
+      margin-left: .06rem;
+      line-height: .2rem;
+      font-size: .12rem;
+      color: $light-fontColor;
+      text-decoration: line-through;
+    }
+
+    .product__number {
+      position: absolute;
+      right: 0;
+      bottom: .12rem;
+
+      &__minus, &__plus {
+        display: inline-block;
+        width: .2rem;
+        height: .2rem;
+        border-radius: 50%;
+        font-size: .2rem;
+        text-align: center;
+        line-height: .16rem;
+      }
+
+      &__minus {
+        border: .01rem solid $medium-fontColor;
+        color: $medium-fontColor;
+        margin-right: .05rem;
+      }
+
+      &__plus {
+        background: $btn-bgColor;
+        color: $bgColor;
+        margin-left: .05rem;
+      }
+    }
+  }
+}
+</style>
